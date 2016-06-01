297edff41970f9ba0abc87779f81073a3f3acc39
Leo Yuuki Omori Omi <ra138684@bart.lab.ic.unicamp.br>
2016-06-01 13:56:19 -0300

    exercise 5 done

--- exercicio6/client_udp.c
+++ exercicio6/client_udp.c
@@ -16,7 +16,8 @@ int main(int argc, char * argv[]){
   struct hostent *hp;
   struct sockaddr_in sin, server_info;
   char *host;
-  char buf[MAX_LINE];
+  char in_buf[MAX_LINE];
+  char out_buf[MAX_LINE];
   int s;
   int len;
   socklen_t addr_size, addr_size_server;
@@ -51,18 +52,17 @@ int main(int argc, char * argv[]){
   addr_size = sizeof sin;
   
   /* main loop: get and send lines of text */
-  while (fgets(buf, sizeof(buf), stdin)) {
-    buf[MAX_LINE-1] = '\0';
-    len = strlen(buf) + 1;
-    sendto(s, buf, len, 0, (struct sockaddr *)&sin, addr_size);  
-    len = recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *)&server_info, &addr_size_server);
+  while (fgets(in_buf, sizeof(in_buf), stdin)) {
+    in_buf[MAX_LINE-1] = '\0';
+    len = strlen(in_buf) + 1;
+    sendto(s, in_buf, len, 0, (struct sockaddr *)&sin, addr_size);
+
+    /*while loop that ignores messages from sources that are not the specified echo server*/
+    do{
+      len = recvfrom(s, out_buf, sizeof(out_buf), 0, (struct sockaddr *)&server_info, &addr_size_server);
+    }while(strcmp(host, inet_ntoa(server_info.sin_addr)) != 0 || (int)ntohs(server_info.sin_port) != SERVER_PORT);
+    
     printf("From IP: %s, port: %d\n", inet_ntoa(server_info.sin_addr), (int)ntohs(server_info.sin_port));
-    if(strcmp(host, inet_ntoa(server_info.sin_addr)) == 0 && (int)ntohs(server_info.sin_port) == SERVER_PORT){
-      fputs(buf, stdout);
-      printf("%s", buf);}
-    else{
-      fputs(buf, devnull);
-    }
+    fputs(out_buf, stdout);
   }
-  
 }
